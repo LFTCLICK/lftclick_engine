08946f1acb4e00d571cd639e572cfeba3f9e38ff
vahowald <vahowald@gmail.com>
2022-02-18 15:58:24 -0800

    Added more objects for milestone

---
+++ LFT Click Engine/Components/Bullet.cpp
@@ -0,0 +1,45 @@
+// ---------------------------------------------------------------------------
+// Project Name		:	LFTClick Engine
+// File Name		:	AnimationHelper.cpp
+// Author			:	Vance Howald
+// Creation Date	:	2021/12/10
+// Purpose			:	Animates grabables
+// History			: 
+// ---------------------------------------------------------------------------
+#include "Bullet.h"
+#include "../FrameRateControler.h"
+
+
+void Bullet::Start()
+{
+	trans = parent->getComponent<Transform>();
+	timer = 0;
+}
+
+void Bullet::Update()
+{
+	if (timer >= animationTime)
+	{
+		timer = 0;
+		speed *= -1;
+	}
+	trans->Move(0, speed * FrameRateControler::getInstance().DeltaTime());
+	
+	timer += FrameRateControler::getInstance().DeltaTime();
+}
+
+Component* Bullet::Clone(GameObject* newParent)
+{
+	Bullet* toReturn = new Bullet();
+	toReturn->parent = newParent;
+	toReturn->animationTime = animationTime;
+	toReturn->speed = speed;
+	toReturn->timer = 0;
+	return toReturn;
+}
+
+Bullet::Bullet(json j, GameObject* parent) : parent(parent)
+{
+	animationTime = j["animationTime"];
+	speed = j["speed"];
+}
